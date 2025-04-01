1e913280ba8979d687da98570ee59b9145537333
Anthony Gallop <agallop@google.com>
2025-04-01 11:54:39 -0700

    Add release.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     id("com.google.devtools.ksp")
 }
 
-
 android {
     namespace = "org.ransomsensei"
     compileSdk = 35
@@ -24,7 +23,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
